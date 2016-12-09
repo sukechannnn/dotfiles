f549b90db1e583107b4c6870791c45ecd825d64f
yosuke0315 <wonderfulworld0315@gmail.com>
2016-12-09 19:16:32 +0900

    add localRubyGems.sh to install gems global

---
+++ localRubyGems.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+gem install bundler
+gem install pry
+gem install pry-byebug
+gem install pry-doc
+gem install pry-stack_explorer
+gem install rubocop
+gem install rsense
+gem install ruby-debug-ide
+gem install debase
