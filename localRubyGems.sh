f0212524eb9b0d389720738f45ad6b5dd7a80d32
yosuke0315 <wonderfulworld0315@gmail.com>
2017-03-31 18:08:00 +0900

    Add gems 'tapp' and 'awesome_print'

--- localRubyGems.sh
+++ localRubyGems.sh
@@ -5,6 +5,8 @@ gem install pry
 gem install pry-byebug
 gem install pry-doc
 gem install pry-stack_explorer
+gem install tapp
+gem install awesome_print
 gem install rubocop
 gem install rsense
 gem install ruby-debug-ide
